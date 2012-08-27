45dd916680cb434b0d3e2129db92b30c547337ea
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-08-27 16:26:32 -0400

    Added support for -T flag

--- cowsaysay.sh
+++ cowsaysay.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 2 -a $# -ne 4 ]
 then
-	echo "Usage: $0 <n> <text>"
+	echo "Usage: $0 [-T tongue] <n> <text>"
 	exit 1
+elif [ $# -eq 4 -a $1 != '-T' ]
+then
+	echo "Usage: $0 [-T tongue] <n> <text>"
+        exit 1
 fi
 
-if [ $1 -eq 0 ]
+if [ $# -eq 2 ]
 then
-	echo $2 | cowsay
+	ARGS=-n
+	TEXT=$2
+	NUM=$1
+else
+	ARGS="-n -T $2"
+	TEXT=$4
+	NUM=$3
+fi
+
+if [ $NUM -eq 0 ]
+then
+	echo $TEXT $ARGS | cowsay
 	exit 0
 fi
 
 TMP1=/tmp/cowsaysay$$_1.tmp
 TMP2=/tmp/cowsaysay$$_2.tmp
 
-echo $2 > $TMP1
+echo $TEXT > $TMP1
 
-for i in `seq 1 $1`
+for i in `seq 1 $NUM`
 do
-	cowsay -n <$TMP1 >$TMP2	
+	cowsay $ARGS <$TMP1 >$TMP2	
 	mv $TMP2 $TMP1
 done
 
